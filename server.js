659301642371dc47e19ee1c15a29bfd331cf0707
KELLY LUU <kellyluuu@gmail.com>
2022-04-06 00:39:31 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const drinks = require('./models/drinks.js'); 
+
+app.listen(3000,()=>console.log('Welcome to the Gitpub App!'))
+
+app.get("/",(req,res)=>{
+    res.send('Welcome to the Gitpub App!')
+})
+
+app.get("/drinks",(req,res)=>{
+    res.send(drinks)
+})
