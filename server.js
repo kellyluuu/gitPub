b7f7747baa632497c573e99e6ae5d98740f3411f
KELLY LUU <kellyluuu@gmail.com>
2022-04-07 01:25:42 -0500

    updated first page "/"

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const food = require('./models/food.js')
 app.listen(3000,()=>console.log('Welcome to the Gitpub App!'))
 
 app.get("/",(req,res)=>{
-    res.send('<html><body><h1>Welcome to the Gitpub App!</h1><a href="./drinks">Drinks Index</a></body></html>')
+    res.send('<html> <head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></head><body class="m-4"><h1>Welcome to the Gitpub App!</h1><a href="./drinks">Drinks Index</a><br><a href="./foods">Foods Index</a></body></html>')
 })
 
 app.get("/foods",(req,res)=>{
