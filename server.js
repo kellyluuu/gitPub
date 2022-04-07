69707983c2d80dd07843719842588bb85150f123
KELLY LUU <kellyluuu@gmail.com>
2022-04-07 01:14:23 -0500

    add food and css

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
 const drinks = require('./models/drinks.js'); 
+const food = require('./models/food.js')
+
 
 
 app.listen(3000,()=>console.log('Welcome to the Gitpub App!'))
 
 app.get("/",(req,res)=>{
-    res.send('Welcome to the Gitpub App!')
+    res.send('<html><body><h1>Welcome to the Gitpub App!</h1><a href="./drinks">Drinks Index</a></body></html>')
+})
+
+app.get("/foods",(req,res)=>{
+    res.render('foods_index.ejs', {allFoods: food})
+})
+
+app.get("/foods/:id", (req, res)=>{
+    res.render("foods_show.ejs", {food: food[req.params.id]})
 })
 
 app.get("/drinks",(req,res)=>{
